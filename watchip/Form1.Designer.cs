11d83a2d1f2435ccf7d8e164a35acca312ace60c
unknown <42616@PSSL.COM.CN>
2017-04-08 18:21:01 +0800

    添加项目文件。

---
+++ watchip/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace watchip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(220, 209);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "向IT报修";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 208);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 33);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "检查网络";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 16F);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(14, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 147);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoEllipsis = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(9, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(337, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "您的IP地址如下";
+            // 
+            // label3
+            // 
+            this.label3.AutoEllipsis = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label3.Location = new System.Drawing.Point(111, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 30);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "网络状态";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(254, 48);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = " 加载S盘";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 251);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IP测试查看工具";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ watchip/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Specialized;
+using System.Text;
+namespace watchip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.label3.Text = null;
+            this.label1.Text = ShowIP();
+
+        }
+        private string ShowIP()
+        {
+            string ips = null;
+            //ipv4地址也可能不止一个
+            foreach (string ip in GetLocalIpv4())
+            {
+                ips += ip.ToString() + '\n';
+            }
+            return ips;
+        }
+        private string[] GetLocalIpv4()
+        {
+            //事先不知道ip的个数，数组长度未知，因此用StringCollection储存
+            IPAddress[] localIPs;
+            localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            StringCollection IpCollection = new StringCollection();
+            foreach (IPAddress ip in localIPs)
+            {
+                //根据AddressFamily判断是否为ipv4,如果是InterNetWorkV6则为ipv6
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    IpCollection.Add(ip.ToString());
+            }
+            string[] IpArray = new string[IpCollection.Count];
+            IpCollection.CopyTo(IpArray, 0);
+            return IpArray;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = ShowIP();
+            bool stat = false;
+            stat = multiping();
+            if (stat)
+            {
+                System.Diagnostics.Process.Start("http://10.65.202.79:8001/Login.aspx");
+            } else
+            {
+                MessageBox.Show("计算机网络不通，请拨打63333");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = ShowIP();
+            bool stat = false;
+            stat = multiping();
+            if (stat == true)
+            {
+                this.label3.Text = "网络正常";
+            }
+            else
+            {
+                this.label3.Text = "网络不通";
+                MessageBox.Show("计算机网络不通，请拨打63333");
+            }
+        }
+    
+    private bool multiping()
+    {
+        bool st1 = false;
+        bool st2 = false;
+        st1 = Ping("10.65.1.53");
+        st2 = Ping("10.65.1.51");
+        if (st1 == true || st2 == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool Ping(string ip)
+    {
+
+        System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+        System.Net.NetworkInformation.PingOptions options = new System.Net.NetworkInformation.PingOptions();
+        options.DontFragment = true;
+        string data = "Test Data!";
+        byte[] buffer = Encoding.ASCII.GetBytes(data);
+        int timeout = 1000; // Timeout 时间，单位：毫秒
+        try
+        {
+            System.Net.NetworkInformation.PingReply reply = p.Send(ip, timeout, buffer, options);
+            if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+                return true;
+            else
+                return false;
+        }
+        catch
+        {
+        }
+        return false;
+
+
+    }
+
+    }
+}
