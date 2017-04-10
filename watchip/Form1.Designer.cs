671950fe3c8dbbc1cadabbf973d7c6043e60cf67
unknown <42616@PSSL.COM.CN>
2017-04-10 11:42:36 +0800

    resize button

--- watchip/Form1.Designer.cs
+++ watchip/Form1.Designer.cs
@@ -40,10 +40,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(220, 209);
+            this.button1.Location = new System.Drawing.Point(241, 207);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.Size = new System.Drawing.Size(75, 33);
             this.button1.TabIndex = 0;
             this.button1.Text = "向IT报修";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,10 +51,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(14, 208);
+            this.button2.Location = new System.Drawing.Point(12, 207);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(76, 33);
+            this.button2.Size = new System.Drawing.Size(75, 33);
             this.button2.TabIndex = 0;
             this.button2.Text = "检查网络";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,7 +67,7 @@
             this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.label1.Location = new System.Drawing.Point(14, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(237, 147);
+            this.label1.Size = new System.Drawing.Size(239, 147);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
@@ -87,7 +87,7 @@
             this.label3.AutoEllipsis = true;
             this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.label3.Location = new System.Drawing.Point(111, 211);
+            this.label3.Location = new System.Drawing.Point(115, 211);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(103, 30);
             this.label3.TabIndex = 1;
@@ -95,9 +95,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(249, 48);
+            this.button3.Location = new System.Drawing.Point(241, 46);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(80, 33);
+            this.button3.Size = new System.Drawing.Size(75, 33);
             this.button3.TabIndex = 2;
             this.button3.Text = " 加载S盘";
             this.button3.UseVisualStyleBackColor = true;
@@ -107,7 +107,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 251);
+            this.ClientSize = new System.Drawing.Size(343, 251);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
